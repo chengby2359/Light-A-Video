b30fdad43586e5942ee1e640050e83fd55f62023
chengby2359 <chengby2359@gmail.com>
2025-03-17 10:25:40 +0000

    update

--- scripts/i2v.sh
+++ scripts/i2v.sh
@@ -2,9 +2,9 @@
 
 
 
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
+# SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-python ${SCRIPT_DIR}/i2v.py /mnt/nas/datasets_tmp/ffhq/images1024x1024/04000/04033.png \
+python mytools/i2v.py /mnt/nas/datasets_tmp/ffhq/images1024x1024/04000/04033.png \
                             /mnt/nas/share2/home/cby/Light-A-Video/input_wan/04033.mp4 \
                             --resolution 512 512 \
                             --frame_count 13 \
---
+++ scripts/video_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+# SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+python mytools/video_info.py input_cog/bear.mp4
