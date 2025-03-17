19c9dcfd96e20652186c4148acbc4937263a0d54
chengby2359 <chengby2359@gmail.com>
2025-03-17 09:15:23 +0000

    update

---
+++ scripts/lav_relight.sh
@@ -0,0 +1,4 @@
+python lav_relight.py --config "configs/relight/car.yaml" \
+                        --sd_model "/inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/zhoujianjun-240108120125/chengby/models/stablediffusionapi/realistic-vision-v51" \
+                        --motion_adapter_model "/inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/zhoujianjun-240108120125/chengby/models/animatediff-motion-adapter-v1-5-3" \
+                        --ic_light_model "/inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/zhoujianjun-240108120125/chengby/models/lllyasviel/ic-light/iclight_sd15_fc.safetensors"
