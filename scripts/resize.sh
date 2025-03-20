d046ed3dac42791409bd2f886507dff3aa2fec48
chengby2359 <chengby2359@gmail.com>
2025-03-20 02:39:53 +0000

    update

---
+++ scripts/resize.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+# SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+python mytools/resize.py /inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/zhoujianjun-240108120125/chengby/datasets/PPR10K/converted_cby/train/input/221_8_0.png \
+                            /inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/zhoujianjun-240108120125/chengby/datasets/PPR10K/converted_cby/1024/221_8.png \
+                            --width 1024 \
+                            --height 1024 \
